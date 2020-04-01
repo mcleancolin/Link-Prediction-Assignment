45b76c5ba5b0cae42576d217c65aa4e894d9b1c9
Colin McLean <mclean.c@husky.neu.edu>
2020-04-01 15:44:42 +1100

    initial training

--- link_predictions.py
+++ link_predictions.py
@@ -1,14 +1,75 @@
-import numpy as np, json, csv
+import pandas as pd
+import numpy as np
+import networkx as nx
+import csv, random
 
 # Loading in the file into a dictionary with the json library
 def open_file():
+
+    with open('train.txt', 'r') as f:
+        lines = f.readlines()
+
+    lines = [line.replace(' ', '\t') for line in lines]
+
+    with open('train.txt', 'w') as f:
+        f.writelines(lines)
+
     with open('train.txt') as f:
-        content = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    content = [x.strip() for x in content]
-    return content
+        reader = csv.reader(f, delimiter="\t")
+        d = list(reader)
+
+    data = []
+    for row in d:
+        data.append([ int(x) for x in row ])
+    return data
+
+
+
+def second_processing(content):
+    node_list_1 = []
+    node_list_2 = []
+    for row in content:
+        for connection in range(0,len(row)):
+            if connection == 0:
+                continue
+            else:
+                node_list_1.append(row[0])
+                node_list_2.append(row[connection])
+
+    df = pd.DataFrame({'node_1': node_list_1, 'node_2': node_list_2})
+
+    return df
+
+def create_graph(edge_list):
+    graph = nx.from_pandas_edgelist(edge_list, "node_1", "node_2", create_using=nx.Graph())
+    n = graph.number_of_nodes()
+    m = graph.number_of_edges()
+    print("Number of nodes :", str(n))
+    print("Number of edges :", str(m))
+    print("Number of connected components :" + str(nx.number_connected_components(graph)))
+    return graph
+
+def trim_graph(graph):
+    edge_subset = random.sample(graph.edges(), int(0.10 * graph.number_of_edges()))
+    graph_train = graph.copy()
+    graph_train.remove_edges_from(edge_subset)
+    return edge_subset, graph_train
+
+def jaccard_coefficient(edge_subset, graph):
+    predictions_preferential = nx.jaccard_coefficient(graph, edge_subset)
+    print("predictions made!")
+    scores = {}
+    for u,v,p in predictions_preferential:
+        scores[(u,v)] = p
+        print("The score for edge " + str(u) + "," + str(v) + " is " + str(p))
+
+
+    print("scores/labels calculated")
 
 
 if __name__ == "__main__":
     data = open_file()
-    print(data[2][0])
+    edge_list = second_processing(data)
+    graph = create_graph(edge_list)
+    edge_subset, graph_train = trim_graph(graph)
+    jaccard_coefficient(edge_subset, graph_train)
