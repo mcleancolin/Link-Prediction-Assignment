051dd39963579e28aaf8ce69da7e2ae0a3ed9417
Colin McLean <mclean.c@husky.neu.edu>
2020-04-01 11:57:21 +1100

    creating the script for link predictions

---
+++ link_predictions.py
@@ -0,0 +1,15 @@
+import numpy, json
+
+# Loading in the file into a dictionary with the json library
+def open_file():
+    data = []
+
+    with open('train.txt') as data_file:
+        for line in data_file:
+            # load
+            except:
+                continue
+
+    return data
+
+print("initial file")
