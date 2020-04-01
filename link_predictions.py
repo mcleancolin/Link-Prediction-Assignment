1730fd0f5a1cd6be5090bc647fc7735413eda58d
Colin McLean <mclean.c@husky.neu.edu>
2020-04-01 19:33:28 +1100

    successfully predict test data

--- link_predictions.py
+++ link_predictions.py
@@ -19,9 +19,18 @@ def open_file():
         d = list(reader)
 
     data = []
+    max = 1
     for row in d:
         data.append([ int(x) for x in row ])
     return data
+
+def find_max_id(test_data):
+    max_value = 1
+    for row in test_data:
+        if max(row) > max_value:
+            max_value = max(row)
+
+    print(max_value)
 
 def open_test_file():
     input = pd.read_csv('test-public.csv')
@@ -59,13 +68,16 @@ def generate_training_edge_list(content):
     return training_edge_list
 
 def create_graph(edge_list):
-    graph = nx.from_pandas_edgelist(edge_list, "node_1", "node_2", create_using=nx.Graph())
-    n = graph.number_of_nodes()
-    m = graph.number_of_edges()
+    graph1 = nx.from_pandas_edgelist(edge_list, "node_1", "node_2")
+    graph2 = nx.path_graph(4084)
+    graph2.add_edges_from(graph1.edges())
+    n = graph2.number_of_nodes()
+    m = graph2.number_of_edges()
     print("Number of nodes :", str(n))
     print("Number of edges :", str(m))
-    print("Number of connected components :" + str(nx.number_connected_components(graph)))
-    return graph
+    print("Number of connected components :" + str(nx.number_connected_components(graph2)))
+
+    return graph2
 
 def trim_graph(graph):
     edge_subset = random.sample(graph.edges(), int(0.10 * graph.number_of_edges()))
@@ -74,30 +86,25 @@ def trim_graph(graph):
     return edge_subset, graph_train
 
 def jaccard_coefficient(graph, test_edge_list):
-    predictions_preferential = nx.jaccard_coefficient(graph, test_edge_list)
+    predictions_preferential = nx.adamic_adar_index(graph, test_edge_list)
 
     scores = {}
-    count = 0
+    for u, v, p in predictions_preferential:
+        scores[(u,v)] = p
+        print(u, v, p)
 
-    for u in predictions_preferential:
-
-
-        #print(u)
-        count += 1
-
-        #scores[(u,v)] = p
-    print(count)
 
     print("scores/labels calculated")
     return scores
 
 
 if __name__ == "__main__":
-    test_data = open_test_file()
-    test_edge_list = genereate_test_edge_list(test_data)
-
     training_data = open_file()
+    max_id = find_max_id(training_data)
     training_edge_list = generate_training_edge_list(training_data)
     graph = create_graph(training_edge_list)
+
+    test_data = open_test_file()
+    test_edge_list = genereate_test_edge_list(test_data)
 
     scores = jaccard_coefficient(graph, test_edge_list)
